8d4eec34e1827016e4362c83fc22425693ba3393
Samuel <samuelbuma8@gmail.com>
2024-11-18 13:24:21 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="text-center">
+      <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white text-[calc(10px+2vmin)]">
+        <img 
+          src="logo.svg" 
+          className="h-[40vmin] pointer-events-none animate-spin-slow" 
+          alt="logo" 
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+        <a 
+          className="text-sky-400" 
+          href="https://reactjs.org" 
+          target="_blank" 
           rel="noopener noreferrer"
         >
           Learn React
